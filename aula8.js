184179eb7af1691074aa5c02f0d14b1b61a30271
renanbneto <renanbneto@hotmail.com>
2024-08-09 21:34:04 -0300

    9 agosto

--- aula8.js
+++ aula8.js
@@ -1,7 +1,37 @@
 
 
-jQuery(documenr).ready(function()){
+jQuery(document).ready(function () {
 
     jQuery("h1").css("color", "red");
 
-}
+    jQuery(".bt-add").click(function () {
+
+        var cidade = $("#cidade").val();
+
+        var lista = $("#lista").html();
+
+        if (cidade != "") {
+            if (lista.search(cidade) == -1) {
+
+                jQuery("#lista").append("<li>" + cidade);
+
+                $("#cidade").val("");
+
+            }
+
+            else (lista == lista)
+            {
+                alert("Esta cidade já foi cadastrada!");
+            }
+
+            
+            else (cidade == "")
+            {
+                alert("Por favor, insira uma cidade!");
+            }
+
+        });
+
+});
+
+
